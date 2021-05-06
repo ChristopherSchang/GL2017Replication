f9fd8dba6e2fb06a12f1ee03adb15a462779e094
ChristopherSchang <christopher.c.schang@gmail.com>
2021-05-06 14:15:43 +0200

    Tests run
    Some toy-example tests run

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,18 @@
 using Test, GL2017Replication
 
-@test hello("Julia") == "Hello, Julia"
-@test domath(2.0) ≈ 7.0
+@testset "Testing the testing" begin
+    @test hello("Julia") == "Hello, Julia"
+    @test domath(2.0) ≈ 7.0 
+end
+
+@testset "Structures" begin
+    gl = ModelGL()
+    @test typeof(gl.β) == Float64 
+end
+
+@testset "initilize" begin
+    gl = ModelGL() 
+    prior_pr = copy(gl.pr)
+    initilize!(gl)
+    @test sum(gl.pr) ≈ 1.0 
+end
